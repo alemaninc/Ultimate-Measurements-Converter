5770e6565e6f60696b5c643890b58024932888ed
alemaninc <97172850+alemaninc@users.noreply.github.com>
2022-01-11 21:04:10 +0000

    Added Acceleration

--- main.js
+++ main.js
@@ -136,6 +136,127 @@ function absorbeddoseconverter() {
 
 
 
+// Acceleration starts here
+var accelerationnumberinput = 1
+var accelerationunitinput = document.getElementById("accelerationunitinput")
+var accelerationnumberoutput = 1
+var accelerationunitoutput = document.getElementById("accelerationunitoutput")
+function getaccelerationcoefficient(x) {
+  if (x=="angstroms per second²)" {
+    return 1e-10
+  } else if (x=="astronomical units per second²)" {
+    return 149597870700
+  } else if (x=="attometers per second²)" {
+    return 1e-18
+  } else if (x=="cana per second²)" {
+    return 2
+  } else if (x=="centimeters per second²)" {
+    return 0.01
+  } else if (x=="chains per second²)" {
+    return 20.1168
+  } else if (x=="Chinese chi per second²)" {
+    return 0.33333333333333333
+  } else if (x=="cubits per second²)" {
+    return 0.4572
+  } else if (x=="decameters per second²)" {
+    return 10
+  } else if (x=="decimeters per second²)" {
+    return 0.1
+  } else if (x=="Earth radii per second²)" {
+    return 6378100
+  } else if (x=="exameters per second²)" {
+    return 1e18
+  } else if (x=="fathoms per second²)" {
+    return 1.8288
+  } else if (x=="feet per second²)" {
+    return 0.3048
+  } else if (x=="femtometers per second²)" {
+    return 1e-15
+  } else if (x=="furlongs per second²)" {
+    return 201.168
+  } else if (x=="gigameters per second²)" {
+    return 1e9
+  } else if (x=="hectometers per second²)" {
+    return 100
+  } else if (x=="Hong Kong chi per second²)" {
+    return 0.371475
+  } else if (x=="Hubble lengths per second²)" {
+    return 1.3640557e26
+  } else if (x=="inches per second²)" {
+    return 0.0254
+  } else if (x=="kilometers per second²)" {
+    return 1000
+  } else if (x=="leagues per second²)" {
+    return 5556
+  } else if (x=="li per second²)" {
+    return 500
+  } else if (x=="light years per second²)" {
+    return 9460528412464108
+  } else if (x=="links per second²)" {
+    return 0.201168
+  } else if (x=="lunar distances per second²)" {
+    return 384399000
+  } else if (x=="megameters per second²)" {
+    return 1000000
+  } else if (x=="meters per second²)" {
+    return 1
+  } else if (x=="micrometers per second²)" {
+    return 0.000001
+  } else if (x=="miles per second²)" {
+    return 1609.344
+  } else if (x=="millimeters per second²)" {
+    return 0.001
+  } else if (x=="nanometers per second²)" {
+    return 1e-9
+  } else if (x=="nautical miles per second²)" {
+    return 1852
+  } else if (x=="observable universes per second²)" {
+    return 879980510813761469728000000
+  } else if (x=="parsecs per second²)" {
+    return 3.0856775815e16
+  } else if (x=="petameters per second²)" {
+    return 1e15
+  } else if (x=="picometers per second²)" {
+    return 1e-12
+  } else if (x=="planck lengths per second²)" {
+    return 1.616255e-35
+  } else if (x=="rods per second²)" {
+    return 5.0292
+  } else if (x=="shaku per second²)" {
+    return 0.30303030303030303
+  } else if (x=="terameters per second²)" {
+    return 1e12
+  } else if (x=="yards per second²)" {
+    return 0.9144
+  } else if (x=="yoctometers per second²)" {
+    return 1e-24
+  } else if (x=="yottameters per second²)" {
+    return 1e24
+  } else if (x=="zeptometers per second²)" {
+    return 1e-21
+  } else if (x=="zettameters per second²)" {
+    return 1e21
+  } else if (x=="meters per hour²") {
+    return 0.00027777777778
+  } else if (x=="kilometers per hour²") {
+    return 0.27777777777778
+  } else if (x=="miles per hour²") {
+    return 0.44704
+  } else {
+    return 1
+  }
+}
+function accelerationconverter() {
+  accelerationnumberinput = document.getElementById("accelerationnumberinput").value
+  accelerationunitinput = getaccelerationcoefficient(document.getElementById("accelerationunitinput").value)
+  accelerationunitoutput = getaccelerationcoefficient(document.getElementById("accelerationunitoutput").value)
+  accelerationnumberoutput = accelerationnumberinput*accelerationunitinput/accelerationunitoutput
+  document.getElementById("accelerationnumberoutput").innerHTML = format(accelerationnumberoutput)
+}
+
+
+
+
 // Length starts here
 var lengthnumberinput = 1
 var lengthunitinput = document.getElementById("lengthunitinput")
@@ -345,6 +466,8 @@ function timeconverter() {
   document.getElementById("timenumberoutput").innerHTML = format(timenumberoutput)
 }
 function loop() {
+  absorbeddoseconverter()
+  accelerationconverter()
   lengthconverter()
   timeconverter()
 }
